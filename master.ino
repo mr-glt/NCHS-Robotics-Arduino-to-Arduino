ec7477ba73b5e4ef001dbf6ae798adfec76df975
stitchens <stevenhitchens31099@gmail.com>
2016-09-22 12:36:05 -0400

    Only one LED rather than multiple
    Actually Fixed Now

--- master.ino
+++ master.ino
@@ -29,7 +29,7 @@ void receiveEvent(int howMany) {
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x
+    byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(y);              // sends one byte
     Wire.endTransmission();    // stop transmitting
@@ -40,40 +40,40 @@ void receiveEvent(int howMany) {
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x
+    byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(y);              // sends one byte
     Wire.endTransmission();    // stop transmitting
     delay(500);
   }
   if (x==2){
-    digitalWrite(LED1, HIGH);
+    digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x
+    byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(y);              // sends one byte
     Wire.endTransmission();    // stop transmitting
     delay(500);
   }
   if (x==3){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, LOW);
-    byte y = x
+    byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(y);              // sends one byte
     Wire.endTransmission();    // stop transmitting
     delay(500);
   }
   if (x==4){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
     digitalWrite(LED4, HIGH);
-    byte y = x
+    byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
     Wire.write(y);              // sends one byte
     Wire.endTransmission();    // stop transmitting
