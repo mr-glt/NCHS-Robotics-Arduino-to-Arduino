6760b14476fb0285ee7d3abf7cc5d29b92586b48
Charlie Wade <colson72098@gmail.com>
2016-09-22 12:06:23 -0400

    Add led code

--- slave.ino
+++ slave.ino
@@ -13,15 +13,62 @@ void receiveEvent(int howMany) {
   while (1 < Wire.available()) {
     char c = Wire.read();
     Serial.print(c);
-
   }
   int x = Wire.read();
   Serial.println(x);
-  send(x);
-}
-void send(int gotValue){
-  byte y = gotValue+1;
-  Wire.beginTransmission(9); // transmit to device #8
-  Wire.write(y);              // sends one byte
-  Wire.endTransmission();    // stop transmitting
+  if (x==0){
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==1){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==2){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==3){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==4){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    byte y = x
+    Wire.beginTransmission(9); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
 }
