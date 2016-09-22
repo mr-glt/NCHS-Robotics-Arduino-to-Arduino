b33a160741b9ec9695700e91a20c24c9b4fb6d25
stitchens <stevenhitchens31099@gmail.com>
2016-09-22 11:52:40 -0400

    Made Functional
    Random (x) input, LED output, same (x) output.

--- master.ino
+++ master.ino
@@ -1,13 +1,17 @@
 #include <Wire.h>
+int LED1=3;
+int LED2=5;
+int LED3=9;
+int LED4=10;
 
 void setup() {
   Wire.begin(9);
   Wire.onReceive(receiveEvent);
+  Serial.begin(9600);
 }
 
-byte x = 0; //what we send
-
 void loop() {
+  byte x = random(0,5); //what we send
   Wire.beginTransmission(8); // transmit to device #8
   Wire.write(x);              // sends one byte
   Wire.endTransmission();    // stop transmitting
@@ -20,5 +24,60 @@ void receiveEvent(int howMany) {
   }
   int x = Wire.read();
   Serial.println(x);
+  if (x==0){
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==1){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==2){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, LOW);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==3){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, LOW);
+    byte y = x
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
+  if (x==4){
+    digitalWrite(LED1, HIGH);
+    digitalWrite(LED2, HIGH);
+    digitalWrite(LED3, HIGH);
+    digitalWrite(LED4, HIGH);
+    byte y = x
+    Wire.beginTransmission(8); // transmit to device #8
+    Wire.write(y);              // sends one byte
+    Wire.endTransmission();    // stop transmitting
+    delay(500);
+  }
   delay(1000);
 }
