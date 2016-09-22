cbe942c60fa1550225485a82c76a7d0400785d69
Charlie Wade <colson72098@gmail.com>
2016-09-22 16:28:16 -0400

    Working rebuild

--- slave.ino
+++ slave.ino
@@ -3,74 +3,54 @@ int LED1=3;
 int LED2=5;
 int LED3=9;
 int LED4=10;
+int x = 0;
+int y = 0;
 void setup() {
-  Wire.begin(8);
-  Wire.onReceive(receiveEvent);
   Serial.begin(9600);
+  Wire.begin(9);
+  Wire.onReceive(receiveEvent);
   pinMode(LED1, OUTPUT);
   pinMode(LED2, OUTPUT);
   pinMode(LED3, OUTPUT);
   pinMode(LED4, OUTPUT);
 }
-
-void loop() {
-  delay(1);
+void receiveEvent(int bytes) {
+  y = Wire.read();
+  Serial.print(y);
 }
-void receiveEvent(int howMany) {
-  while (1 < Wire.available()) {
-    char c = Wire.read();
-    Serial.print(c);
-  }
-  int x = Wire.read();
-  Serial.println(x);
-  if (x==0){
+void loop(){
+  x = random(0,5);
+  Wire.beginTransmission(8); // transmit to device #9
+  Wire.write(x);              // sends x
+  Wire.endTransmission();    // stop transmitting
+  if(y==0){
     digitalWrite(LED1, LOW);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x;
-    Wire.beginTransmission(9); // transmit to device #8
-    Wire.write(y);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
   }
-  if (x==1){
+  if(y==1){
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, LOW);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x;
-    Wire.beginTransmission(9); // transmit to device #8
-    Wire.write(y);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
   }
-  if (x==2){
+  if(y==2){
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
-    byte y = x;
-    Wire.beginTransmission(9); // transmit to device #8
-    Wire.write(y);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
   }
-  if (x==3){
+  if(y==3){
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, LOW);
-    byte y = x;
-    Wire.beginTransmission(9); // transmit to device #8
-    Wire.write(y);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
   }
-  if (x==4){
+  if(y==4){
     digitalWrite(LED1, HIGH);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, HIGH);
-    byte y = x;
-    Wire.beginTransmission(9); // transmit to device #8
-    Wire.write(y);              // sends one byte
-    Wire.endTransmission();    // stop transmitting
   }
 }
