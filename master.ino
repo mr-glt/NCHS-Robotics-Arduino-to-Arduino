f0586721cd580854eedfa7c3ffc50cdee2ed0a1f
stitchens <stevenhitchens31099@gmail.com>
2016-09-22 12:56:11 -0400

    Only one LED rather than multiple (#2)
    Actually Fixed Now

--- master.ino
+++ master.ino
@@ -52,7 +52,7 @@ void receiveEvent(int howMany) {
     delay(500);
   }
   if (x==2){
-    digitalWrite(LED1, HIGH);
+    digitalWrite(LED1, LOW);
     digitalWrite(LED2, HIGH);
     digitalWrite(LED3, LOW);
     digitalWrite(LED4, LOW);
@@ -63,8 +63,8 @@ void receiveEvent(int howMany) {
     delay(500);
   }
   if (x==3){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
     digitalWrite(LED3, HIGH);
     digitalWrite(LED4, LOW);
     byte y = x;
@@ -74,9 +74,9 @@ void receiveEvent(int howMany) {
     delay(500);
   }
   if (x==4){
-    digitalWrite(LED1, HIGH);
-    digitalWrite(LED2, HIGH);
-    digitalWrite(LED3, HIGH);
+    digitalWrite(LED1, LOW);
+    digitalWrite(LED2, LOW);
+    digitalWrite(LED3, LOW);
     digitalWrite(LED4, HIGH);
     byte y = x;
     Wire.beginTransmission(8); // transmit to device #8
