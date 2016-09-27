046c2ae2c4a66eab389fb6e4e16ef48c4db039dd
Steven Hitchens <shitchens2017@NCHS-DOM.com>
2016-09-27 09:53:09 -0400

    Started Comms

--- wire/master.ino
+++ wire/master.ino
@@ -1,14 +1,21 @@
 //Hi Steven
 int led1 = 1;
-int led1 = 2;
-int led1 = 3;
-int led1 = 4;
+int led2 = 2;
+int led3 = 3;
+int led4 = 4;
 
 void setup() {
   pinMode(led1, OUTPUT);
   pinMode(led2, OUTPUT);
   pinMode(led3, OUTPUT);
   pinMode(led4, OUTPUT);
+
+  pinMode(A1, INPUT);
+  pinMode(A2, INPUT);
+  pinMode(A3, INPUT);
+  pinMode(A4, INPUT);
+
+  x = random (0,5);
 }
 
 void void loop() {
