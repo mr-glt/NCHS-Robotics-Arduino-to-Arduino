3a762a6a6106a5063b5886a98068b922b3a1ad8c
Charlie Wade <colson72098@gmail.com>
2016-09-27 09:37:25 -0400

    Leds

--- wire/master.ino
+++ wire/master.ino
@@ -1,7 +1,14 @@
 //Hi Steven
+int led1 = 1;
+int led1 = 2;
+int led1 = 3;
+int led1 = 4;
 
 void setup() {
-
+  pinMode(led1, OUTPUT);
+  pinMode(led2, OUTPUT);
+  pinMode(led3, OUTPUT);
+  pinMode(led4, OUTPUT);
 }
 
 void void loop() {
